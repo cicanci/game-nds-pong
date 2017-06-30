a34917ff8ed141f8ec59dd2ea9cfbeff63d4d221
Bruno Cicanci <brunocicanci@gmail.com>
2017-06-30 00:24:06 -0300

    Updated README and added a better way to init the video

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 
 Pong using [devkitPro](https://devkitpro.org/) for Nintendo DS.
 
+## Tools
+
+[Atom](http://atom.io)
+
+[GCC Make Run Package for Atom](https://atom.io/packages/gcc-make-run)
+
+[DeSmuME - Nintendo DS emulator](http://desmume.org/)
+
 ## Links
 
-[Xcode setup for devkitPro](http://wiibrew.org/wiki/Devkitppc_setup_(Mac_OS_X))
+[libnds documentation](http://libnds.devkitpro.org/)
 
-[Nintendo DS emulator](http://desmume.org/)
+[Xcode setup for devkitPro](http://wiibrew.org/wiki/Devkitppc_setup_(Mac_OS_X)
+
+[NDS Homebrew VRAM Banks Selector](https://mtheall.com/banks.html)
+
+[NDS Homebrew VRAM BG Allocation Conflict Viewer](https://mtheall.com/vram.html)
--- source/main.cpp
+++ source/main.cpp
@@ -18,108 +18,148 @@ typedef struct
     int paletteAlpha;
     int x;
     int y;
-}MySprite;
+} SpriteInfo;
+
+void initVideo()
+{
+  /*
+   *  Map VRAM to display a background on the main and sub screens.
+   *
+   *  The vramSetPrimaryBanks function takes four arguments, one for each of
+   *  the major VRAM banks. We can use it as shorthand for assigning values to
+   *  each of the VRAM bank's control registers.
+   *
+   *  We map banks A and B to main screen  background memory. This gives us
+   *  256KB, which is a healthy amount for 16-bit graphics.
+   *
+   *  We map bank C to sub screen background memory.
+   *
+   *  We map bank D to LCD. This setting is generally used for when we aren't
+   *  using a particular bank.
+   *
+   *  We map bank E to main screen sprite memory (aka object memory).
+   */
+  vramSetPrimaryBanks(VRAM_A_MAIN_BG_0x06000000,
+                      VRAM_B_MAIN_BG_0x06020000,
+                      VRAM_C_SUB_BG_0x06200000,
+                      VRAM_D_LCD);
+
+  vramSetBankE(VRAM_E_MAIN_SPRITE);
+
+  /*  Set the video mode on the main screen. */
+  videoSetMode(MODE_5_2D | // Set the graphics mode to Mode 5
+         //DISPLAY_BG2_ACTIVE | // Enable BG2 for display
+         DISPLAY_BG3_ACTIVE | // Enable BG3 for display
+         DISPLAY_SPR_ACTIVE | // Enable sprites for display
+         DISPLAY_SPR_1D     // Enable 1D tiled sprites
+         );
+
+  /*  Set the video mode on the sub screen. */
+  videoSetModeSub(MODE_5_2D | // Set the graphics mode to Mode 5
+          DISPLAY_BG3_ACTIVE); // Enable BG3 for display
+}
+
+void initBackground()
+{
+  bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+  decompress(backgroundBitmap, BG_GFX, LZ77Vram);
+
+  bgInitSub(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
+  decompress(backgroundBitmap, BG_GFX_SUB, LZ77Vram);
+}
 
 int main(void)
 {
-//    int i = 0;
-    int x = 0;
-    int y = 150;
-    
-    videoSetMode(MODE_5_2D);
-    videoSetModeSub(MODE_5_2D);
-    
-    vramSetBankA(VRAM_A_MAIN_BG);
-    //vramSetBankA(VRAM_A_MAIN_SPRITE);
-    vramSetBankC(VRAM_C_SUB_BG);
-    vramSetBankD(VRAM_D_SUB_SPRITE);
-    
-    // Backgrounds
-    
-    bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-    decompress(backgroundBitmap, BG_GFX, LZ77Vram);
-    
-    bgInitSub(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-    decompress(backgroundBitmap, BG_GFX_SUB, LZ77Vram);
+    /*  Turn on the 2D graphics core. */
+    powerOn(POWER_ALL_2D);
+
+    initVideo();
+    initBackground();
 
     // Players
-    
     oamInit(&oamMain, SpriteMapping_Bmp_1D_128, false);
     oamInit(&oamSub, SpriteMapping_Bmp_1D_128, false);
-    
-    MySprite red = {0, SpriteSize_32x32, SpriteColorFormat_256Color, 0, 0, 0, 0};
-    red.gfx = oamAllocateGfx(&oamMain, red.size, red.format);
-    dmaCopy(redBitmap, red.gfx, 32*32);
-    
-    MySprite blue = {0, SpriteSize_32x32, SpriteColorFormat_256Color, 0, 0, 0, 150};
-    blue.gfx = oamAllocateGfx(&oamSub, blue.size, blue.format);
-    dmaCopy(blueBitmap, blue.gfx, 32*32);
+
+    SpriteInfo redPlayer = {0, SpriteSize_32x32, SpriteColorFormat_256Color, 0, 0, 0, 0};
+    redPlayer.gfx = oamAllocateGfx(&oamMain, redPlayer.size, redPlayer.format);
+    dmaCopy(redBitmap, redPlayer.gfx, 32*32);
+
+    SpriteInfo bluePlayer = {0, SpriteSize_32x32, SpriteColorFormat_256Color, 0, 0, 0, 150};
+    bluePlayer.gfx = oamAllocateGfx(&oamSub, bluePlayer.size, bluePlayer.format);
+    dmaCopy(blueBitmap, bluePlayer.gfx, 32*32);
 
 //    u16* gfx = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
 //    u16* gfxSub = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_256Color);
-//    
+//
+//    int i = 0;
 //    for(i = 0; i < 32 * 32 / 2; i++)
 //    {
 //        gfx[i] = 1 | (1 << 8);
 //        gfxSub[i] = 1 | (1 << 8);
 //    }
-//    
+//
 //    SPRITE_PALETTE[1] = RGB15(31, 31, 31);
 //    SPRITE_PALETTE_SUB[1] = RGB15(31, 31, 31);
-    
+
+    SPRITE_PALETTE[1] = RGB15(0, 31, 0);
+    SPRITE_PALETTE_SUB[1] = RGB15(0, 31, 0);
+
+    int x = 0;
+    int y = 150;
+
     while(1)
     {
         scanKeys();
-        
+
         int held = keysHeld();
 
         if(held & KEY_LEFT)
         {
             x -= SPEED;
         }
-        
+
         if(held & KEY_RIGHT)
         {
             x += SPEED;
         }
-        
+
         oamSet(&oamMain, //main graphics engine context
                0,           //oam index (0 to 127)
                0, 0, //touch.px, touch.py,   //x and y pixle location of the sprite
                0,                    //priority, lower renders last (on top)
                0,					  //this is the palette index if multiple palettes or the alpha value if bmp sprite
-               red.size, //SpriteSize_32x32,
-               red.format, //SpriteColorFormat_256Color,
-               red.gfx, //gfx,                  //pointer to the loaded graphics
+               redPlayer.size, //SpriteSize_32x32,
+               redPlayer.format, //SpriteColorFormat_256Color,
+               redPlayer.gfx, //gfx,                  //pointer to the loaded graphics
                -1,                  //sprite rotation data
                false,               //double the size when rotating?
                false,			//hide the sprite?
                false, false, //vflip, hflip
                false	//apply mosaic
                );
-        
-        
-        oamSet(&oamSub, 
+
+
+        oamSet(&oamSub,
                0,
                x,
                y,
-               0, 
                0,
-               blue.size,
-               blue.format,
-               blue.gfx,
-               -1, 
-               false, 
-               false,			
-               false, false, 
-               false	
-               );              
-        
+               0,
+               bluePlayer.size,
+               bluePlayer.format,
+               bluePlayer.gfx,
+               -1,
+               false,
+               false,
+               false, false,
+               false
+               );
+
         swiWaitForVBlank();
-        
+
         oamUpdate(&oamMain);
         oamUpdate(&oamSub);
     }
-    
+
     return 0;
 }
