db0ed50c2f6c3a1e27f3984bb76e77853d820668
Bruno Cicanci <brunocicanci@gmail.com>
2018-06-26 00:44:23 -0300

    Changing PONG to game of life

--- source/main.cpp
+++ source/main.cpp
@@ -2,164 +2,247 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <string.h>
 
 #include "background.h"
 #include "blue.h"
 #include "red.h"
 
-const int SPEED = 4;
+unsigned char** newUniv = NULL;
 
-typedef struct
-{
-    u16* gfx;
-    SpriteSize size;
-    SpriteColorFormat format;
-    int rotationIndex;
-    int paletteAlpha;
-    int x;
-    int y;
-} SpriteInfo;
-
-void initVideo()
-{
-  /*
-   *  Map VRAM to display a background on the main and sub screens.
-   *
-   *  The vramSetPrimaryBanks function takes four arguments, one for each of
-   *  the major VRAM banks. We can use it as shorthand for assigning values to
-   *  each of the VRAM bank's control registers.
-   *
-   *  We map banks A and B to main screen  background memory. This gives us
-   *  256KB, which is a healthy amount for 16-bit graphics.
-   *
-   *  We map bank C to sub screen background memory.
-   *
-   *  We map bank D to LCD. This setting is generally used for when we aren't
-   *  using a particular bank.
-   *
-   *  We map bank E to main screen sprite memory (aka object memory).
-   */
-  vramSetPrimaryBanks(VRAM_A_MAIN_BG_0x06000000,
-                      VRAM_B_MAIN_BG_0x06020000,
-                      VRAM_C_SUB_BG_0x06200000,
-                      VRAM_D_LCD);
-
-  vramSetBankE(VRAM_E_MAIN_SPRITE);
-
-  /*  Set the video mode on the main screen. */
-  videoSetMode(MODE_5_2D | // Set the graphics mode to Mode 5
-         //DISPLAY_BG2_ACTIVE | // Enable BG2 for display
-         DISPLAY_BG3_ACTIVE | // Enable BG3 for display
-         DISPLAY_SPR_ACTIVE | // Enable sprites for display
-         DISPLAY_SPR_1D     // Enable 1D tiled sprites
-         );
-
-  /*  Set the video mode on the sub screen. */
-  videoSetModeSub(MODE_5_2D | // Set the graphics mode to Mode 5
-          DISPLAY_BG3_ACTIVE); // Enable BG3 for display
-}
-
-void initBackground()
-{
-  bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-  decompress(backgroundBitmap, BG_GFX, LZ77Vram);
-
-  bgInitSub(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-  decompress(backgroundBitmap, BG_GFX_SUB, LZ77Vram);
-}
-
-int main(void)
-{
-    /*  Turn on the 2D graphics core. */
-    powerOn(POWER_ALL_2D);
-
-    initVideo();
-    initBackground();
-
-    // Players
-    oamInit(&oamMain, SpriteMapping_Bmp_1D_128, false);
-    oamInit(&oamSub, SpriteMapping_Bmp_1D_128, false);
-
-    SpriteInfo redPlayer = {0, SpriteSize_32x32, SpriteColorFormat_256Color, 0, 0, 0, 0};
-    redPlayer.gfx = oamAllocateGfx(&oamMain, redPlayer.size, redPlayer.format);
-    dmaCopy(redBitmap, redPlayer.gfx, 32*32);
-
-    SpriteInfo bluePlayer = {0, SpriteSize_32x32, SpriteColorFormat_256Color, 0, 0, 0, 150};
-    bluePlayer.gfx = oamAllocateGfx(&oamSub, bluePlayer.size, bluePlayer.format);
-    dmaCopy(blueBitmap, bluePlayer.gfx, 32*32);
-
-//    u16* gfx = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
-//    u16* gfxSub = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_256Color);
-//
-//    int i = 0;
-//    for(i = 0; i < 32 * 32 / 2; i++)
-//    {
-//        gfx[i] = 1 | (1 << 8);
-//        gfxSub[i] = 1 | (1 << 8);
-//    }
-//
-//    SPRITE_PALETTE[1] = RGB15(31, 31, 31);
-//    SPRITE_PALETTE_SUB[1] = RGB15(31, 31, 31);
-
-    SPRITE_PALETTE[1] = RGB15(0, 31, 0);
-    SPRITE_PALETTE_SUB[1] = RGB15(0, 31, 0);
-
-    int x = 0;
-    int y = 150;
-
-    while(1)
-    {
-        scanKeys();
-
-        int held = keysHeld();
-
-        if(held & KEY_LEFT)
-        {
-            x -= SPEED;
+void show(unsigned char** univ, int w, int h) {
+    system("clear");
+    //printf("\033[H");
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            //printf(univ[y][x] ? "\033[07m  \033[m" : "  ");
+            printf(univ[y][x] ? "0" : "  ");
         }
-
-        if(held & KEY_RIGHT)
-        {
-            x += SPEED;
-        }
-
-        oamSet(&oamMain, //main graphics engine context
-               0,           //oam index (0 to 127)
-               0, 0, //touch.px, touch.py,   //x and y pixle location of the sprite
-               0,                    //priority, lower renders last (on top)
-               0,					  //this is the palette index if multiple palettes or the alpha value if bmp sprite
-               redPlayer.size, //SpriteSize_32x32,
-               redPlayer.format, //SpriteColorFormat_256Color,
-               redPlayer.gfx, //gfx,                  //pointer to the loaded graphics
-               -1,                  //sprite rotation data
-               false,               //double the size when rotating?
-               false,			//hide the sprite?
-               false, false, //vflip, hflip
-               false	//apply mosaic
-               );
-
-
-        oamSet(&oamSub,
-               0,
-               x,
-               y,
-               0,
-               0,
-               bluePlayer.size,
-               bluePlayer.format,
-               bluePlayer.gfx,
-               -1,
-               false,
-               false,
-               false, false,
-               false
-               );
-
-        swiWaitForVBlank();
-
-        oamUpdate(&oamMain);
-        oamUpdate(&oamSub);
+        //printf("\033[E");
     }
+    fflush(stdout);
+}
 
+unsigned char** empty_univ(int w, int h) {
+    unsigned char** univ = (unsigned char **) malloc(h * sizeof(unsigned char*));
+    for(int i = 0; i < h; i++) {
+        univ[i] = (unsigned char *)malloc(w * sizeof(unsigned char));
+        for (int j = 0; j < w; j++) {
+            univ[i][j] = 0;
+        }
+    }
+    
+    return univ;
+}
+
+void evolve(unsigned char ** univ, int w, int h) {
+    if (NULL == newUniv) {
+        newUniv = empty_univ(w, h);
+    }
+    
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            
+            int n = 0;
+            for (int y1 = y - 1; y1 <= y + 1; y1++) {
+                for (int x1 = x - 1; x1 <= x + 1; x1++) {
+                    // Any cell outside the bounding box is considered to be dead.
+                    if (univ[(y1 + h) % h][(x1 + w) % w]) {
+                        n++;
+                    }
+                }
+            }
+            
+            if (univ[y][x]) {
+                n--;
+            }
+            newUniv[y][x] = (n == 3 || (n == 2 && univ[y][x]));
+            
+        }
+    }
+    
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            univ[y][x] = newUniv[y][x];
+        }
+    }
+}
+
+void game(int w, int h, unsigned char** univ, int cycles, int print_result, int display_timer) {
+    int c = 0;
+    while (c < cycles) {
+        if (display_timer > 0) {
+            show(univ, w, h);
+            //usleep(display_timer);
+            for (int i = 0; i < display_timer; i++) {
+                swiWaitForVBlank();
+            }
+        }
+        evolve(univ, w, h);
+        c++;
+    }
+    // Should we print the universe when simulation is over?
+    if (1 == print_result) {
+        show(univ, w, h);
+    }
+    printf("Simulation completed after %d cycles.\n\n", cycles);
+}
+
+unsigned char** random_univ(int w, int h) {
+    unsigned char** univ = empty_univ(w, h);
+    
+    int seed = time(NULL);
+    srand(seed);
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            if (rand() < (RAND_MAX / 5)) {
+                univ[y][x] = 1;
+            } else {
+                univ[y][x] = 0;
+            }
+        }
+    }
+    return univ;
+}
+
+unsigned char** read_from_file(char* filename, int* w, int* h) {
+    FILE* file = fopen(filename, "r");
+    char line[1024];
+    
+    if (file == NULL) {
+        return NULL;
+    }
+    
+    if(fgets(line, sizeof(line), file)) {
+        char *currnum;
+        int numbers[2], i = 0;
+        
+        while ((currnum = strtok(i ? NULL : line, " ,")) != NULL && i < 2) {
+            numbers[i++] = atoi(currnum);
+        }
+        
+        *h = numbers[0];
+        *w = numbers[1];
+    }
+    
+    unsigned char** univ = empty_univ(*w, *h);
+    
+    for (int y = 0; y < *h; y++) {
+        fgets(line, sizeof(line), file);
+        for (int x = 0; x < *w; x++) {
+            if (line[x] == '1') {
+                univ[y][x] = 1;
+            }
+        }
+    }
+    
+    fclose(file);
+    
+    return univ;
+}
+
+int main2(int c, char **v) {
+    if (c < 2) {
+        printf("Invalid number of arguments.\n");
+        return -1;
+    }
+    
+    int w = 0, h = 0, cycles = 10, display_timer = 0, print_result = 0, num_threads = 0;
+    unsigned char** univ;
+    
+    if (0 == strcmp(v[1], "random")) {
+        printf("Using randomly generated pattern.\n");
+        
+        if (c > 2) w = atoi(v[2]);
+        if (c > 3) h = atoi(v[3]);
+        if (c > 4) cycles = atoi(v[4]);
+        if (c > 5) num_threads = atoi(v[5]);
+        if (c > 6) print_result = atoi(v[6]);
+        if (c > 7) display_timer = atoi(v[7]);
+        
+        if (w <= 0) w = 30;
+        if (h <= 0) h = 30;
+        
+        univ = random_univ(w, h);
+    } else if (0 == strcmp(v[1], "file")) {
+        if (c < 3) {
+            printf("Missing file path.\n");
+            return -1;
+        }
+        
+        univ = read_from_file(v[2], &w, &h);
+        
+        if (NULL == univ) {
+            printf("Invalid input file.\n");
+            return -1;
+        }
+        
+        if (c > 3) cycles = atoi(v[3]);
+        if (c > 4) num_threads = atoi(v[4]);
+        if (c > 5) print_result = atoi(v[5]);
+        if (c > 6) display_timer = atoi(v[6]);
+    } else {
+        printf("Missing arguments.\n");
+        return -1;
+    }
+    
+    if (cycles <= 0) cycles = 10;
+    if (display_timer < 0) display_timer = 0;
+    
+    if (print_result < 0) print_result = 0;
+    else if (print_result > 1) print_result = 1;
+    
+    if (num_threads <= 0) num_threads = 1;
+    
+    game(w, h, univ, cycles, print_result, display_timer);
+    
+    return 0;
+}
+
+//---------------------------------------------------------------------------------
+int main(void) {
+    //---------------------------------------------------------------------------------
+    touchPosition touch;
+    
+    PrintConsole topScreen;
+    PrintConsole bottomScreen;
+    
+    videoSetMode(MODE_0_2D);
+    videoSetModeSub(MODE_0_2D);
+    
+    vramSetBankA(VRAM_A_MAIN_BG);
+    vramSetBankC(VRAM_C_SUB_BG);
+    
+    consoleInit(&topScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+    consoleInit(&bottomScreen, 3, BgType_Text4bpp, BgSize_T_256x256, 31, 0, false, true);
+    
+    consoleSelect(&topScreen);
+//    iprintf("\n\n\tHello DS dev'rs\n");
+//    iprintf("\twww.drunkencoders.com\n");
+//    iprintf("\twww.devkitpro.org");
+    
+    consoleSelect(&bottomScreen);
+    
+    int w = 10;
+    int h = 10;
+    game(w, h, random_univ(w, h), 100, 1, 1);
+    
+    while(1) {
+        
+        touchRead(&touch);
+        
+        //printf("\x1b[10;0HTouch x = %04i, %04i\n", touch.rawx, touch.px);
+        //iprintf("Touch y = %04i, %04i\n", touch.rawy, touch.py);
+        
+        swiWaitForVBlank();
+        scanKeys();
+        
+        int keys = keysDown();
+        
+        if(keys & KEY_A) break;
+
+    }
+    
     return 0;
 }
